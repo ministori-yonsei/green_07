092658b3207cf758002d4e5c9d682c71e8fd5505
ministori-yonsei <74632170+ministori-yonsei@users.noreply.github.com>
2021-07-22 12:32:43 +0900

    Add files via upload

--- fe_app/js/function.js
+++ fe_app/js/function.js
@@ -85,24 +85,47 @@ $(function(){
         });
 
     /*
+    * 현재 사용자 위치 데이터
+    * */
+    let currentLatitude = 0; // 0으로 초기화
+    let currentLongitude = 0; // 0으로 초기화
+
+    function showPosition(position) {
+        currentLatitude = position.coords.latitude;
+        currentLongitude = position.coords.longitude;
+    }
+
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+    /*
     * 지도 데이터
     * */
-    var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-        mapOption = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
-        };
 
-    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    var map = new kakao.maps.Map(mapContainer, mapOption);
+    var mapContainer;
+    var map;
+    var marker;
 
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        position: map.getCenter()
-    });
+    function showMap(currentLatitude, currentLongitude){
+        mapContainer = document.getElementById('map'), // 지도를 표시할 div
+            mapOption = {
+            //33.450701, 126.570667
+                center: new kakao.maps.LatLng(currentLatitude, currentLongitude), // 지도의 중심좌표
+                level: 3 // 지도의 확대 레벨
+            };
 
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
+        // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+        map = new kakao.maps.Map(mapContainer, mapOption);
+
+        // 마커를 생성합니다
+        marker = new kakao.maps.Marker({
+            position: map.getCenter()
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+    }
+
+    showMap(33.450701, 126.570667);
 
     // 지도에 클릭 이벤트를 등록합니다
     // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
@@ -121,4 +144,39 @@ $(function(){
         console.log(latlng.getLng());
 
     });
+
+    $('.current-button').on('click', function(){
+        showMap(currentLatitude, currentLongitude);
+    });
+
+    $('.search-button').on('click',function(){
+        let inputValue = $('.search-text').val();
+
+        search(inputValue); // 함수 호출
+    });
+
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    function search(inputValue){
+        // 주소로 좌표를 검색합니다
+        geocoder.addressSearch(inputValue, function(result, status) {
+
+            // 정상적으로 검색이 완료됐으면
+            if (status === kakao.maps.services.Status.OK) {
+
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: coords
+                });
+
+                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                map.setCenter(coords);
+            }
+        });
+    }
+
 });
